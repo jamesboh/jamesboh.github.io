988200ad6012484dc439d82d364c151ec3278260
James Oh <jamesboh@gmail.com>
2015-08-16 23:56:48 -0400

    updates

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,27 @@
-function init() {
-	// defer images
-	var imgDefer = document.getElementsByTagName('img');
-	
-	for (var i=0; i<imgDefer.length; i++) {
-		if(imgDefer[i].getAttribute('data-src')) {
-			imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-		} 
-	} 
+var joby = {
+
+	init: function(){
+		joby.deferImages();
+		joby.animations();
+	},
+
+	deferImages: function() {
+		var imgDefer = document.getElementsByTagName('img');
+		
+		for (var i=0; i<imgDefer.length; i++) {
+			if(imgDefer[i].getAttribute('data-src')) {
+				imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
+			} 
+		} 			
+	},
+
+	animations: function() {
+		// amberidentity animation
+		var screenshot = $('#amberidentity img');
+		TweenMax.to(screenshot, 30, {marginTop: '-235%', ease: Power0.easeNone, repeat:-1, yoyo:true});
+	}
+
+
 }
-window.onload = init;
+
+window.onload = joby.init();
